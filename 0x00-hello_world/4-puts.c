d654fe4169e10185195ec5bf426c07c57e5922ce
Joshua Banjo <yjbanjo23@gmail.com>
2022-12-08 16:12:00 -0800

    One Nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,13 @@
  *  * main - Entry point
  *   *
  *    * Return: always 0
- *     */
+ *     
+ *     
+ *   
+ *
+ *
+ */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
