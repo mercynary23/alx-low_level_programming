c8cd1d78d8105ea185531a447dc82767c139cfdb
Joshua Banjo <yjbanjo23@gmail.com>
2022-12-13 18:28:34 -0800

    Try1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
  */
 int main(void)
 {
-	char arr[] = "_putchar";
-	int i = 0;
-
-	while (arr[i] != '\0')
-	{
-		_putchar(arr[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
